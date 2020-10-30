b2db90f639351eee867d70fe60ea599590933ade
Andrew <andrew_case11@mymail.eku.edu>
2020-10-30 18:16:06 -0400

    added questionable vr support, created second scene

---
+++ Assets/ballgame.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballgame : MonoBehaviour
+{
+
+    public int point;
+    public GameObject ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        point = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            point += 50;
+            Debug.Log("got points");
+            Instantiate(ball, new Vector3(25, -4, 1), Quaternion.identity);
+        }
+    }
+
+}
+
---
+++ Assets/points.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skeeball : MonoBehaviour
+{
+
+    public int points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        points = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            points += 50;
+            Debug.Log("got points");
+        }
+    }
+
+}
